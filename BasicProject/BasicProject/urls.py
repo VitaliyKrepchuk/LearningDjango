c8fa1d505b1a4f9522e8aee91f0697fcd1e5f565
VitaliyKrepchuk <vitalijkrepcuk@gmail.com>
2023-03-28 19:41:30 +0300

    Pages

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -4,5 +4,6 @@ import HelloDjangoApp.views
 # Django processes URL patterns in the order they appear in the array
 urlpatterns = [
     re_path(r'^$', HelloDjangoApp.views.index, name='index'),
-    re_path(r'^home$', HelloDjangoApp.views.index, name='home')
+    re_path(r'^home$', HelloDjangoApp.views.index, name='home'),
+    re_path(r'^about$', HelloDjangoApp.views.about, name='about'),
 ]
